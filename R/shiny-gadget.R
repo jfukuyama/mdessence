8d2f7fbf83dc2c4cd95ab1802fd5b6838e914ab6
Julia Fukuyama <julia.fukuyama@gmail.com>
2019-07-25 09:16:51 -0400

    add covariate support for biplot, closes #6

--- R/shiny-gadget.R
+++ R/shiny-gadget.R
@@ -2,7 +2,13 @@
 #'
 #' @import shiny
 #' @export
-interactiveBiplot <- function(Y, dist, dist_deriv = NULL, k = 2, axes = 1:2, samples = NULL, ...) {
+interactiveBiplot <- function(Y, dist, dist_deriv = NULL, k = 2, axes = 1:2, samples = NULL,
+                              sample_data = NULL,
+                              sample_mapping = aes_string(x = "Axis1", y = "Axis2"),
+                              sample_facet = NULL,
+                              var_data = NULL,
+                              var_mapping = aes_string(x = "xend", y = "yend"),
+                              layout = c(5,5), ...) {
     dist_fns = make_dist_fns(dist, dist_deriv)
     mds_matrices = make_mds_matrices(Y, dist_fns$dist_fn)
     embedding_and_sample_num = data.frame(mds_matrices$X, sample_num = 1:nrow(mds_matrices$X))
@@ -10,20 +16,28 @@ interactiveBiplot <- function(Y, dist, dist_deriv = NULL, k = 2, axes = 1:2, sam
     varnames = get_variable_names(mds_matrices$Y)
     ui <- fluidPage(
         headerPanel("Interactive Sensitivity Biplot"),
-        sidebarPanel(width = 2,
+        sidebarPanel(width = 1,
                      actionButton("done", "Done")),
-        mainPanel(width = 10,
+        mainPanel(width = 11,
                   fluidPage(
                       fluidRow(
-                          column(5, plotOutput("plot_samples", click = "plot_click")),
-                          column(5, plotOutput("plot_variables")))
+                          column(layout[1], plotOutput("plot_samples", click = "plot_click")),
+                          column(layout[2], plotOutput("plot_variables")))
                   )
                   )
     )
 
     server <- function(input, output, session) {
         output$plot_samples = renderPlot({
-            ggplot(embedding_and_sample_num) + geom_point(aes(x = Axis1, y = Axis2))
+            if(!is.null(sample_data))
+                p = ggplot(data.frame(embedding_and_sample_num, sample_data), sample_mapping) +
+                    geom_point()
+                else
+                    p = ggplot(embedding_and_sample_num, sample_mapping) +
+                        geom_point()
+                if(!is.null(sample_facet))
+                    p = p + sample_facet
+                p
         })
         output$plot_variables = renderPlot({
             biplot_center = nearPoints(embedding_and_sample_num, input$plot_click, maxpoints = 1)
@@ -37,7 +51,12 @@ interactiveBiplot <- function(Y, dist, dist_deriv = NULL, k = 2, axes = 1:2, sam
                 sensitivity = sensitivity_list[[sample_num]][,axes],
                 center = mds_matrices$X[sample_num, axes],
                 sample = sample_num, varnames = varnames)
-            ggplot(biplot_df) + geom_point(aes(x = xend, y = yend))
+            if(!is.null(var_data))
+                p = ggplot(data.frame(biplot_df, var_data), var_mapping) + geom_point()
+            else
+                p = ggplot(biplot_df, var_mapping) +
+                    geom_point()
+            p
         })
 
         observeEvent(input$done, {

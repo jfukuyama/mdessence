0c2c95075df89d32c3ffeb2f8bb4ed1db82261c3
Julia Fukuyama <julia.fukuyama@gmail.com>
2020-09-07 11:07:04 -0400

    add data

---
+++ R/data-doc.R
@@ -0,0 +1,10 @@
+#' Antibiotic time course experiment.
+#'
+#' A phyloseq object describing a time course experiment in which
+#' three people two courses of cipro and had their gut microbiomes
+#' sampled. See Dethlefsen and Relman, PNAS (2010), at
+#' https://www.ncbi.nlm.nih.gov/pubmed/20847294 for more details.
+#'
+#' @format A phyloseq object.
+#' @name abt
+NULL
